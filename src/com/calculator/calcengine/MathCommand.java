a53d32de9138ca434082be7c8cb4d16748095804
Shabiha Fatak <fatakshabiha@gmail.com>
2020-07-01 22:36:43 -0500

    Initial Commit

---
+++ src/com/calculator/calcengine/MathCommand.java
@@ -0,0 +1,9 @@
+package com.calculator.calcengine;
+
+
+enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
---
+++ src/com/calculator/calcengine/MathProcessing.java
@@ -0,0 +1,8 @@
+package com.calculator.calcengine;
+
+public interface MathProcessing {
+    String SEPARATOR = " ";
+    String getKeyword(); // add
+    char getSymbol(); // +
+    double doCalculation(double leftVal, double rightVal);
+}
